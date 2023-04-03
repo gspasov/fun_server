c4a7dbf39874d4df974b09904aea87cbf0bafe54
Georgi Spasov <gvspasov@gmail.com>
2023-04-03 23:59:49 +0300

    feat: Add :ex_doc dependency for generating docs

--- mix.exs
+++ mix.exs
@@ -62,7 +62,10 @@ defmodule FunServer.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+
+      # Documentation dependencies
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
